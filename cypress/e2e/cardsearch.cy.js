49e652e3dc73118dbcb9076a60c6fe11df4453ad
Penelope Schankula <105069331+pnlop@users.noreply.github.com>
2024-07-16 11:53:12 -0400

    ignore exception

--- cypress/e2e/cardsearch.cy.js
+++ cypress/e2e/cardsearch.cy.js
@@ -12,16 +12,19 @@ function scrapeSite(urls) {
     });
     for (const url in urls) {
         it('Scrape site: '+url, () => {
+            Cypress.on('uncaught:exception', (err, runnable) => {
+                // returning false here prevents Cypress from
+                // failing the test
+                return false
+            });
             cy.intercept('GET', '/cart.js').as('load');
             cy.visit(urls[url]);
             cy.wait('@load');
-            cy.wait(1000);
             cy.get('form[action="/search"][method="get"][class*="search-header"]').should('exist').first().within(() => {
                 cy.get('input[type="search"]').should('be.visible').type(Cypress.env('cardData').cardIdentifier);
                 cy.get('button[type="submit"]').should('be.visible').click();
             });
             cy.wait('@load');
-            cy.wait(1000);
             cy.get('.list-view-items').should('exist').children().each(($item, index, $list) => {
                 if ($item.html().length === 0) {
                     cy.wrap($item).invoke('attr', 'data-product-variants').then((variants) => {
