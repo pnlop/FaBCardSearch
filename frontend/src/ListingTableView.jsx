e458fa4fa9aa91fe067ae4d4ecf30dc23973d3e8
Penelope Schankula <105069331+pnlop@users.noreply.github.com>
2024-07-24 13:58:28 -0400

    logs fr

--- frontend/src/ListingTableView.jsx
+++ frontend/src/ListingTableView.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ListingTable from "./ListingTable";
 const ListingTableView = (listings) => {
+    console.log(listings);
     return (
         <div className="listing-table-view">
-            console.log(listings);
             {Object.values(listings.listings).map(listing => (
             <ListingTable listing={listing} url={listings.url}/>
         ))}
