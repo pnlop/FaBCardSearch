0e768fb6b6ae2e44e0408d146bdb8445f438b302
Penelope Schankula <105069331+pnlop@users.noreply.github.com>
2024-07-24 17:26:32 -0400

    s

--- frontend/src/ListingTableView.jsx
+++ frontend/src/ListingTableView.jsx
@@ -8,7 +8,7 @@ const ListingTableView = (listings) => {
     return (
         <div className="listing-table-view">
             {listings.listings.listings.map(listing => (
-            <ListingTable listing={listing} url={listings.listings.url} />
+            <ListingTable listing={listing} url={listings.listings.listings.url} />
         ))}
         </div>
     );
