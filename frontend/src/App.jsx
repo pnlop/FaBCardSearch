303a7a275d58ff9d01156e1cb4bb542a29671aaa
Penelope Schankula <105069331+pnlop@users.noreply.github.com>
2024-07-24 15:45:56 -0400

    FOR

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -57,7 +57,7 @@ function App() {
         },
         body: JSON.stringify(listingRequest),
       })
-        .then((response) => {console.log(response.text());response.json();})
+        .then((response) => {console.log(response.json());response.json();})
         .then((data) => {     
           setPageview(1);
           setListings(data);
