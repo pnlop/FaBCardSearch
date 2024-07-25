8bacbc9c29f4225462484b1057aec4ce68afcfd6
Penelope Schankula <105069331+pnlop@users.noreply.github.com>
2024-07-24 21:07:22 -0400

    qddwq

--- frontend/src/ListingTable.jsx
+++ frontend/src/ListingTable.jsx
@@ -8,6 +8,7 @@ const ListingTable = (listing, url) => {
     return null;
   }
   console.log("listing: " + JSON.stringify(listing));
+  listings.listings.variants = listings.listings.variants.filter((variant) => variant.available);
   const rows = listing.listing.variants.map((variant) => {
       if (!variant.available) {
         return null;
