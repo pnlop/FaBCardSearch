b7d0d6abfeabd978fe7c65e22965cfbeb1dc387d
Penelope Schankula <105069331+pnlop@users.noreply.github.com>
2024-08-08 11:12:57 -0400

    dwq

--- backend/index.ts
+++ backend/index.ts
@@ -187,17 +187,21 @@ async function playwrightScrape(url, cardIdentifier, tcg, tcgAbbr, color) : Prom
     const context = await browser.newContext();
     const page = await context.newPage();
     const template = "You are an HTML parser that returns exclusively well formed JSON and nothing else, all output should begin with '{' and end with '}'. Transform the following HTML store search results for the card " + cardIdentifier + " into structured JSON for storing product information with the following schema:\n" +
-"{\n" +
-"    title: String,\n" +
-"    variants: [\n" +
-"        {\n" +
-"            name: String,\n" +
-"            price: Number,\n" +
-"            available: Boolean,\n" +
-"        }\n" +
-"    ]\n" +
-"}\n" +
-"HTML: ";
+`
+{ listings: [
+    {
+        title: String,
+        variants: [
+            {
+                name: String,
+                price: Number,
+                available: Boolean
+            }
+        ]
+    }
+]
+}
+HTML: `;
     try {
         if (color !== "") {
             cardIdentifier = cardIdentifier + " " + color;
@@ -225,17 +229,21 @@ async function searchURLScrape(url, cardIdentifier, tcg, tcgAbbr, color, searchU
     }
     let response = await axios.get(searchURL+cardIdentifier);
     const template = "You are an HTML parser that returns exclusively well formed JSON and nothing else, all output should begin with '{' and end with '}'. Transform the following HTML store search results for the card " + cardIdentifier + " into structured JSON for storing product information with the following schema:\n" +
-"{\n" +
-"    title: String,\n" +
-"    variants: [\n" +
-"        {\n" +
-"            name: String,\n" +
-"            price: Number,\n" +
-"            available: Boolean,\n" +
-"        }\n" +
-"    ]\n" +
-"}\n" +
-"HTML: ";
+`
+{ listings: [
+    {
+        title: String,
+        variants: [
+            {
+                name: String,
+                price: Number,
+                available: Boolean
+            }
+        ]
+    }
+]
+}
+HTML: `;
     const result = await model.generateContent(template+response.data);
     console.log(result.response.text());
     return {listings: JSON.parse(result.response.text()), url: url};
