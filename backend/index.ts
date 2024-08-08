9af324c97df5cc30ecacf60dffe9893d4a85d116
Penelope Schankula <105069331+pnlop@users.noreply.github.com>
2024-08-08 11:04:54 -0400

    parser spec

--- backend/index.ts
+++ backend/index.ts
@@ -188,7 +188,7 @@ async function playwrightScrape(url, cardIdentifier, tcg, tcgAbbr, color) {
     const browser = await chromium.launch();
     const context = await browser.newContext();
     const page = await context.newPage();
-    const template = "You are an HTML parser. Transform the following HTML store search results for the card " + cardIdentifier + " into structured JSON for storing product information with the following schema:\n" +
+    const template = "You are an HTML parser that returns exclusively well formed JSON and nothing else, all output should begin with '{' and end with '}'. Transform the following HTML store search results for the card " + cardIdentifier + " into structured JSON for storing product information with the following schema:\n" +
 "{\n" +
 "    title: String,\n" +
 "    variants: [\n" +
@@ -220,7 +220,7 @@ async function playwrightScrape(url, cardIdentifier, tcg, tcgAbbr, color) {
 
 async function searchURLScrape(url, cardIdentifier, tcg, tcgAbbr, color, searchURL) {
     let response = await axios.get(searchURL+cardIdentifier);
-    const template = "You are an HTML parser. Transform the following HTML store search results for the card " + cardIdentifier + " into structured JSON for storing product information with the following schema:\n" +
+    const template = "You are an HTML parser that returns exclusively well formed JSON and nothing else, all output should begin with '{' and end with '}'. Transform the following HTML store search results for the card " + cardIdentifier + " into structured JSON for storing product information with the following schema:\n" +
 "{\n" +
 "    title: String,\n" +
 "    variants: [\n" +
