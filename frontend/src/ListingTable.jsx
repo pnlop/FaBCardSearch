9c2dc9706eaa3f9b66e9d4727e0af9c5622d91c6
Penelope Schankula <105069331+pnlop@users.noreply.github.com>
2024-08-14 01:09:40 -0400

    fixup

--- frontend/src/ListingTable.jsx
+++ frontend/src/ListingTable.jsx
@@ -3,7 +3,7 @@ import { Anchor, Card, Table } from "@mantine/core";
 import React from "react";
 import "./ListingTable.css";
 //turn listing table into dynamic table generator per store, iterate over stores/listings in wrapper component
-const ListingTable = (listing, url) => {
+const ListingTable = ({listing, url}) => {
   if (!listing || !listing.listing.variants[0].available) {
     return null;
   }
