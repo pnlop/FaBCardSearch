ecaa0ecbfece595914019d6d9eaa10c6f6d77ace
Penelope Schankula <105069331+pnlop@users.noreply.github.com>
2024-08-14 00:42:51 -0400

    fixup

--- frontend/src/ListingTableView.jsx
+++ frontend/src/ListingTableView.jsx
@@ -7,14 +7,13 @@ const ListingTableView = (listings) => {
         return null;
     }
     console.log("list: " + JSON.stringify(listings));
-    let myUrl = listings.listings.url.toString();
     console.log(myUrl);
     console.log(JSON.parse(listings.listings.listings));
     return (
         <div className="listing-table-view">
             <Text size="xl" fw={700}>{listings.listings.shopName}</Text>
             {JSON.parse(listings.listings.listings).map(listing => (
-            <ListingTable listing={listing} url={myUrl} />
+            <ListingTable listing={listing} />
         ))}
         </div>
     );
